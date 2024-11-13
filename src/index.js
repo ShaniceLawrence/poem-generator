27e9f3e70e9e1071561616dd683094b6305919a4
ShaniceLawrence <Shanicelawrence82@gmail.com>
2024-11-13 12:33:14 +0200

    Added basic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "I cried for you today as I have done many times",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
